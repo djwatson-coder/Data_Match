a0561b03969817e155451f195f72b56793001ccf
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-03-16 16:06:49 -0400

    change constants to uppercase

--- settings.py
+++ settings.py
@@ -1,2 +1,10 @@
+import json
 
-write_table = True
+WRITE_TABLE = True
+PDF_EXTENSION = "/Payments"
+
+f = open("./client_information.json")
+CLIENT_INFORMATION = json.load(f)["client_information"]
+f.close()
+
+EXAMPLE_NAME = "Laureau"
