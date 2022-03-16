6c0e64aafbd9e81e2e4b58eb1955956bc9bf3242
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-03-16 16:06:09 -0400

    Add selection option to main

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import sys
+import settings
+from utils import toolselector
+import readers
 
-from readerselecter import reader_selector
 
+def main(client: str):
+    # Select the formatting functions for the given client
+    attributes = {}
+    for client_info in settings.CLIENT_INFORMATION:
+        if client_info["clientId"] == client:
+            attributes = client_info["attributes"]
+            break
 
-def main(directory: str):
-    reader_selector(directory)
+    payments_reader = getattr(readers.speacialpdfreader, attributes["paymentsReader"])
+    payments_reader = payments_reader(folder_path=attributes["folderPath"], client_name=client)
+
+    # bordereau_reader = getattr(readers.speacialpdfreader, attributes["paymentsReader"])
+    # bordereau_reader.__init__(folder_path=attributes["folderPath"], client_name=client)
+
+    # data_cleaner = select_payments_reader(attributes["redReader"])
+    # data_cleaner.__init__(folder_path=attributes["folderPath"], client_name=client)
+
+    payments_reader.create_table()
+    # bordereau_reader.create_table()
+    # data_cleaner.create_report()
 
 
 if __name__ == '__main__':
 
-    if len(sys.argv) == 2:
+    try:
         main(sys.argv[1])
-    else:
-        main("C:/Users/david.watson/Documents/Clients/ARAG/2022/Lareau")
+    except IndexError:
+        main(settings.EXAMPLE_NAME)
 
 
 
 # ToDo create a command line executable file
 # ToDo create a make command to run the script
+# ToDo add OCR to the PDFReader init varaibles
