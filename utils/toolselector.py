f02d76a4f8efd1f3d5fe4fec895e1ed989699e30
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-03-16 16:06:28 -0400

    Create toolselector.py

---
+++ utils/toolselector.py
@@ -0,0 +1,13 @@
+
+# from readers.speacialpdfreader import *
+#
+#
+# def select_payments_reader(paymentsReader: str):
+#     if paymentsReader == "Pdf_L1":
+#         return LauPDFReader
+#     if paymentsReader == "Pdf_L1":
+#         return VicPDFReader
+#     if paymentsReader == "Pdf_L1":
+#         return LaureauPDFReader
+
+
