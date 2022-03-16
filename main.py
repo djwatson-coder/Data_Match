c4051ebd6876c73882af0f1aaf147375255399bc
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-03-16 10:49:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import sys
 
-from readers.pdfreader import PDFReader
+from readerselecter import reader_selector
 
 
-def main():
-    pdf_reader = PDFReader()
+def main(directory: str):
+    reader_selector(directory)
 
 
 if __name__ == '__main__':
-    print("Hi")
+
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        main("C:/Users/david.watson/Documents/Clients/ARAG/2022/Lareau")
+
 
 
 # ToDo create a command line executable file
