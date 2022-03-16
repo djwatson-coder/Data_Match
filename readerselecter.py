d26c959ee59e25c73da38a1a35986f3fc8226d36
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-03-15 22:30:22 -0400

    Update readerselecter.py

--- readerselecter.py
+++ readerselecter.py
@@ -5,6 +5,7 @@ from readers.agreader import AGPDFReader
 from readers.victorreder import VictorPDFRedear
 
 
+
 def reader_selector(dir):
     reader_type = dir_list = dir.split("/")[-3]
     if reader_type == "Lareau":
