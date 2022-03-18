a8ca5cb9cdc338920c30ac7172e27848dad065e5
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-03-18 17:04:33 -0400

    Update toolselector.py

--- utils/toolselector.py
+++ utils/toolselector.py
@@ -1,13 +1,21 @@
 
-# from readers.speacialpdfreader import *
-#
-#
-# def select_payments_reader(paymentsReader: str):
-#     if paymentsReader == "Pdf_L1":
-#         return LauPDFReader
-#     if paymentsReader == "Pdf_L1":
-#         return VicPDFReader
-#     if paymentsReader == "Pdf_L1":
-#         return LaureauPDFReader
+from readers import speacialpdfreader, excelreader
 
 
+def select_reader(reader_type: str):
+    if "PDFReader" in reader_type:
+        return getattr(speacialpdfreader, reader_type)
+    elif "ExcelReader" == reader_type:
+        return getattr(excelreader, reader_type)
+    elif "ExcelReader" in reader_type:
+        return getattr(excelreader, reader_type)
+
+    return None
+
+
+def get_attributes(client: str, information: dict):
+    for client_info in information:
+        if client_info["clientId"] == client:
+            return client_info["attributes"]
+
+    return None
