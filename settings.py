502514ad6eb9f6382a3bd73bfb79b1774c417b4c
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-03-31 09:50:21 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import json
 
 WRITE_TABLE = True
 PAYMENTS_EXTENSION = "/Payments"
-BORDEREAU_EXTENSION = "/Bordereau"
+BORDEREAU_EXTENSION = "/Bordereau/T2"
 
 f = open("./client_information.json")
 CLIENT_INFORMATION = json.load(f)["client_information"]
