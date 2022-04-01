ed1ee69f106da6fd062a62fd4fcc58d5ec5d8935
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-04-01 15:39:52 -0400

    move table reading and writing to tools

--- utils/toolselector.py
+++ utils/toolselector.py
@@ -1,6 +1,7 @@
 
 from readers import speacialpdfreader, specialexcelreader
-
+import pandas as pd
+import os
 
 def select_reader(reader_type: str):
     if "PDFReader" in reader_type:
@@ -17,4 +18,27 @@ def get_attributes(client: str, information: dict):
             return client_info["attributes"]
 
     return None
+
+
+def read_in_data(client: str, readers: list, file_paths: str):
+    dfs = []
+    sums = []
+    for reader in readers:
+        rdr = select_reader(reader)(file_paths, client)
+        df, summary = rdr.create_table()
+        dfs.append(df)
+        sums.append(summary)
+
+    return pd.concat(dfs, ignore_index=True), pd.concat(sums, ignore_index=True)
+
+
+def write_report(data: dict, save_path: str, report_type: str, client_name: str):
+    path = f"{save_path}/Generated"
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with pd.ExcelWriter(f"{path}/{client_name}_{report_type}.xlsx") as writer:
+        for name, df in data.items():
+            df.to_excel(writer, sheet_name=name, index=False)
+
+    print(f"{report_type} Table Written-----")
 
