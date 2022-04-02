d0404d50d1747ed1d6c8cad2e1d5c6a51b65f258
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-04-01 23:02:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,25 @@
 import settings
 from utils import toolselector as ts
 import pandas as pd
-from datamanip import datamatch as dm
 import sys
 
 
-def main(client: str, create=True):
+def main(client: str, create_data=True):
 
     # Get the attributes of the client
-    attributes = ts.get_attributes(client, settings.CLIENT_INFORMATION)
+    client_info = ts.get_attributes(client, settings.CLIENT_INFORMATION)
 
-    if create:
+    if create_data:
 
         # Read in the Payment Data
         df_pay, df_pay_sum = ts.read_in_data(client,
-                                             attributes["paymentsReaders"],
-                                             attributes["folderPath"] + settings.PAYMENTS_EXTENSION)
+                                             client_info["paymentsReaders"],
+                                             client_info["folderPath"] + settings.PAYMENTS_EXTENSION)
 
         # Read in the Bordereau Data
         df_bord, df_bord_sum = ts.read_in_data(client,
-                                               attributes["bordReaders"],
-                                               attributes["folderPath"] + settings.BORDEREAU_EXTENSION)
+                                               client_info["bordReaders"],
+                                               client_info["folderPath"] + settings.BORDEREAU_EXTENSION)
 
         input_data = {
             "Bordereau Data": df_bord,
@@ -30,27 +29,27 @@ def main(client: str, create=True):
             "Payment Data Summary": df_pay_sum,
         }
 
-        ts.write_report(input_data, attributes["folderPath"], "Input_Data", client)
+        ts.write_report(input_data, client_info["folderPath"], "Input_Data", client)
 
     else:
         # Read in the Excels - to skip the need for the previous code when already generated
-        df_pay = pd.read_excel(attributes["folderPath"] + "/Generated/" + client + "_Input_Data.xlsx",
+        df_pay = pd.read_excel(client_info["folderPath"] + "/Generated/" + client + "_Input_Data.xlsx",
                                sheet_name="Payment Data")
-        df_bord = pd.read_excel(attributes["folderPath"] + "/Generated/" + client + "_Input_Data.xlsx",
+        df_bord = pd.read_excel(client_info["folderPath"] + "/Generated/" + client + "_Input_Data.xlsx",
                                 sheet_name="Bordereau Data")
 
     # Match the Payments and Bordereau Data
-    data_match = dm.DataMatcher()
+    data_match = ts.select_matcher(client)
     summary_report = data_match.create_match_report(df_pay, df_bord)
 
     # Write the final report
-    ts.write_report(summary_report, attributes["folderPath"], "Summary_Report", client)
+    ts.write_report(summary_report, client_info["folderPath"], "Summary_Report", client)
 
     return
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1], False)
 
 # ToDo create a command line executable file
 # ToDo create a make command to run the script
