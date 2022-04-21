0106eeba506523051c8f2f0066625a88cf599e6d
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-04-21 12:21:12 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,8 @@ import json
 
 WRITE_TABLE = True
 PAYMENTS_EXTENSION = "/Payments"
-BORDEREAU_EXTENSION = "/Bordereau/Collated"
+BORDEREAU_EXTENSION = "/Bordereau"
+OUTPUT_SECTION = "C:/Users/david.watson/Documents/Clients/ARAG/0.2 Analysis/Generated files"
 
 f = open("./client_information.json")
 CLIENT_INFORMATION = json.load(f)["client_information"]
