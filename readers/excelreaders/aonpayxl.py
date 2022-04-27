193701363fc4e966949f7a9db9981a61b157a187
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-04-27 12:01:09 -0400

    Update aonpayxl.py

--- readers/excelreaders/aonpayxl.py
+++ readers/excelreaders/aonpayxl.py
@@ -14,15 +14,41 @@ class AonPExcelReader(ExcelReader):
                           "Amount": "APPLIED_AMOUNT"}
         self.folder_path = folder_path
         self.client_name = client_name
-        self.row_skips = 4
+        self.row_skips = 9
 
     def read_file(self, file_path: str):
 
+
         excel_path = f"{self.folder_path}/{file_path}"
-        df = pd.read_excel(excel_path, skiprows=self.row_skips)
+        xl = pd.ExcelFile(excel_path)
+        if 'Remittance Advice' in xl.sheet_names:
+            df = pd.read_excel(excel_path, skiprows=9)
+            df = self.format_type_1(df)
+        elif len(xl.sheet_names) > 1:
+            sheet_names = list(xl.sheet_names)
+            position = self.find_position(file_path, ['Office\nSucc.'], sheet_names[0])
+            df = pd.read_excel(excel_path, skiprows=position, sheet_name=sheet_names[0])
+            for sheet in sheet_names[1:]:
+                position = self.find_position(file_path, ['Office\nSucc.'], sheet)
+                df1 = pd.read_excel(excel_path, skiprows=position, sheet_name=sheet)
+                df = pd.concat([df, df1])
+            df = self.format_type_1(df)
+        else:
+            df = pd.read_excel(excel_path, skiprows=4)
+
         df = self.format_excel(df, file_path)
 
         return df
 
     def format_excel(self, df, file_path: str):
+        df = df[["POLICY_NUMBER", "Named_Insured_Name", "APPLIED_AMOUNT"]]
+
+        return df
+
+    def format_type_1(self, df):
+        """ Skip 9 rows """
+        df = df.rename(columns={"Policy Number\nNo de la Police": "POLICY_NUMBER",
+                                "Client Number\nNo du cliente": "Named_Insured_Name",
+                                "Net Balance\nSolde": "APPLIED_AMOUNT"})
+
         return df
