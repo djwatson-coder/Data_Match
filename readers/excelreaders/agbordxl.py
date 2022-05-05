4853e08e4a9d7a60dd4abebba05e1bdba034ad32
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-05-05 16:57:09 -0400

    Update agbordxl.py

--- readers/excelreaders/agbordxl.py
+++ readers/excelreaders/agbordxl.py
@@ -10,8 +10,10 @@ class AgExcelReader(ExcelReader):
         super(AgExcelReader, self).__init__()
         self.keep_cols = {"Policy": "Broker_Policy_Number",
                           "Company": "Company_Name",
+                          "Effective_Date": "Effective_Date",
                           "File": "File",
-                          "Amount": "Net_Amount"}
+                          "Net_Amount": "Net_Amount",
+                          "Gross_Amount": "Gross_Amount"}
         self.folder_path = folder_path
         self.client_name = client_name
 
@@ -26,7 +28,6 @@ class AgExcelReader(ExcelReader):
             df = self.read_type_file(file_path, sheet=correct_sheet)
 
         df = self.format_excel(df, file_path)
-
         return df
 
     def get_correct_sheet(self, file_path):
@@ -43,59 +44,42 @@ class AgExcelReader(ExcelReader):
         return df
 
     def read_type_file(self, file_path: str, sheet):
-        names = ["Corporate Partner/Broker Policy Number", "Broker Policy Number", "Aon Policy Number",
-                 "Policy No.", "Das Policy Number", "Policy #", "DAS Policy Number", "Certificate No.",
-                 "POLICY_NUMBER"]
+        names = ["Corporate Partner/Broker Policy Number", "Broker Policy Number", "DAS Policy Number"]
         position = self.find_position(file_path, names, sheet)
 
         excel_path = f"{self.folder_path}/{file_path}"
         df = pd.read_excel(excel_path, skiprows=position, sheet_name=sheet, nrows=1000)
 
-        df.columns = df.columns.str.replace(' ', '')
-        df.columns = df.columns.str.replace('/', '')
-        df.columns = df.columns.str.replace('.', '')
-        df.columns = df.columns.str.replace('_', '')
+        df.columns = df.columns.str.replace(r' \(.*\)', '')
+        df.columns = df.columns.str.replace('/', '_')
+        df.columns = df.columns.str.replace('.', '_')
+        df.columns = df.columns.str.replace(' ', '_')
         df.columns = df.columns.str.lower()
 
         # Formatting -- move to a function
         # df = df[df.columns.drop(list(df.filter(regex='Unnamed')))]
-        BPN_Names = ["corporatepartnerbrokerpolicynumber",
-                     "aonpolicynumber",
-                     "corporatepartnernumber",
-                     "daspolicynumber",
-                     "policyno",
-                     "policy#",
-                     "certificateno",
-                     "policynumber"]
-        NAM_Names = ["netpremium",
-                     "daspremium",
-                     "netwrittenpremium",
-                     "daspolicynumber",
-                     "policyno",
-                     "totalpremium",
-                     "19-20daspremium",
-                     "appliedamount",
-                     "grosspremium",
-                     "premium"]  # be careful with this one - should come last
+        BPN_Names = ["broker_policy_number", "corporate_partner_broker_policy_number", "das_policy_number"]
+        EFD_Names = ["cover_from_date"]
+        CPN_Names = ["company_name", "first_name"]
+        NAM_Names = ["net_premium"]
+        GPN_Names = ["gross_premium"]
 
-        df = self.find_rename_columns(df, "brokerpolicynumber", BPN_Names)
-        df = self.find_rename_columns(df, "netamount", NAM_Names)
+        df = self.find_rename_columns(df, "Broker_Policy_Number", BPN_Names)
+        df = self.find_rename_columns(df, "Company_Name", CPN_Names)
+        df = self.find_rename_columns(df, "Effective_Date", EFD_Names)
+        df = self.find_rename_columns(df, "Net_Amount", NAM_Names)
+        df = self.find_rename_columns(df, "Gross_Amount", GPN_Names)
 
-        df = df.rename(columns={"brokerpolicynumber": "Broker_Policy_Number",
-                                "netamount": "Net_Amount"})
-
-        if "Company_Name" not in df.columns:
-            df["Company_Name"] = "No Company Name"
-
-        if "Broker_Policy_Number" not in df.columns or "Net_Amount" not in df.columns:
+        if "Gross_Amount" not in df.columns:
             print(df.columns)
 
-        df = df[["Broker_Policy_Number", "Company_Name", "Net_Amount"]]
+        df = df[["Broker_Policy_Number", "Company_Name", "Effective_Date",
+                 "Net_Amount", "Gross_Amount"]]
 
         return df
 
     def triage_data(self):
-        correct_names = ["Data Table", "AON Municipalities"]
+        correct_names = ["Data Table", "AON Municipalities", "Bordereaux"]
         new_folder = f"{self.folder_path}/Excluded"
         ost.create_directory(new_folder)
         keep_files, remove_files = self.categorise_excel(self.folder_path, ["SAMPLE"], correct_names)
@@ -107,3 +91,6 @@ class AgExcelReader(ExcelReader):
             if col in df.columns and correct_col not in df.columns:
                 df = df.rename(columns={col: correct_col})
         return df
+
+
+
