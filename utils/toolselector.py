d5c691d516387f37b742515f5881717c941c2007
djwatson-coder <65384624+djwatson-coder@users.noreply.github.com>
2022-05-05 16:55:58 -0400

    Update toolselector.py

--- utils/toolselector.py
+++ utils/toolselector.py
@@ -1,14 +1,14 @@
 
-from readers.pdfreaders import agpaypdf, laupaypdf, otherpdfreaders
-from readers.excelreaders import agpayxl, agbordxl, aonpayxl, laubordxl, otherxlreaders
+from readers.pdfreaders import agpaypdf, laupaypdf, otherpdfreaders, bflpaypdf
+from readers.excelreaders import agpayxl, agbordxl, aonpayxl, laubordxl, otherxlreaders, bflbordxl
 from datamanip import datamatch
 
 
 def select_reader(reader_type: str):
     print(reader_type)
-    if "PDFReader" in reader_type:
+    if "pdfreader" in reader_type.lower():
         return select_pdf_reader(reader_type)
-    elif "ExcelReader" in reader_type:
+    elif "excelreader" in reader_type.lower():
         return select_excel_reader(reader_type)
 
     return None
@@ -18,9 +18,9 @@ def select_pdf_reader(reader_type: str):
     pdf_readers = {
         "AgPDFReader": agpaypdf,
         "LaPDfReader": laupaypdf,
+        "BflPayPdfReader": bflpaypdf,
         "other": otherpdfreaders
     }
-
     if reader_type in pdf_readers.keys():
         return getattr(pdf_readers[reader_type], reader_type)
 
@@ -32,6 +32,7 @@ def select_excel_reader(reader_type: str):
         "AgPExcelReader": agpayxl,
         "AonPExcelReader": aonpayxl,
         "AgExcelReader": agbordxl,
+        "BflPExcelReader": bflbordxl
     }
 
     if reader_type in excel_readers.keys():
